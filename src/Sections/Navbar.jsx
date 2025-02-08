00a9fb98407fa165363a96dfa8a8269d209f23f0
Akki-luffy <akhilkompally27@gmail.com>
2025-02-08 14:26:46 +0530

    Logo Added

--- src/Sections/Navbar.jsx
+++ src/Sections/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
     return (
         <>
             <Toaster />
-            <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <nav className="smax-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="container flex h-16 items-center justify-between px-4 mx-auto">
                     <div>
                         <Link
